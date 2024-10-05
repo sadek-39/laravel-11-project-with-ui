18b4bb9e401f30f923969f474cb17c2ea268c9bb
sadek-39 <sadeksltn@gmail.com>
2024-10-05 23:56:48 +0600

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,25 @@
+// import './bootstrap';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,23 @@
+<x-app-layout>
+
+</x-app-layout>
+
+{{--        <!DOCTYPE html>--}}
+{{--<html lang="en">--}}
+{{--<head>--}}
+{{--    <meta charset="utf-8" />--}}
+{{--    <meta http-equiv="X-UA-Compatible" content="IE=edge" />--}}
+{{--    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />--}}
+{{--    <meta name="description" content="" />--}}
+{{--    <meta name="author" content="" />--}}
+{{--    <title>Sidenav Light - SB Admin</title>--}}
+{{--    @vite(['resources/css/app.css', 'resources/js/app.js'])--}}
+{{--    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>--}}
+{{--</head>--}}
+{{--<body class="sb-nav-fixed">--}}
+
+
+{{--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>--}}
+
+{{--</body>--}}
+{{--</html>--}}
